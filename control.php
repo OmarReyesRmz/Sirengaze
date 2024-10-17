272b4845e54fcb91d99dd4922b29629ecbe5edd0
OmarReyesRmz <reyesomar028@gmail.com>
2024-10-17 10:58:03 -0600

    responsitive size

--- control.php
+++ control.php
@@ -7,9 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="css/stytienda.css">
     <style>
-        .apartado i{
-            margin-top:35%;
-            font-size:800%;
+        .apartado i {
+            font-size: 5rem; /* Ajustar el tamaño de los iconos */
             margin-bottom: 10%;
         }
 
@@ -20,8 +19,12 @@
         .apartado {
             border: 2px solid rgb(64, 79, 70);
             border-radius: 15px;
-            height: 115%;
             position: relative;
+            height: 300px; 
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
         }
 
         .apartado i:hover {
@@ -29,7 +32,10 @@
         }
 
         #tienda_apartado {
-            margin-bottom: 3%;
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); /* Distribuir apartados automáticamente */
+            grid-gap: 40px; 
+            margin-bottom: 10%;
         }
 
         .texto {
@@ -57,10 +63,7 @@
             gap: 15px;
         }
 
-        
-
         .botones-container i {
-           
             background-color: rgb(64, 79, 70);
             color: white;
             border-radius: 50%;
@@ -73,13 +76,20 @@
             background-color: rgb(177, 177, 177);;
         }
 
+        @media screen and (max-width: 860px) {
+            #tienda_apartado {
+                grid-template-columns: 1fr; /* Cambiar a una columna en pantallas pequeñas */
+                padding-left: 150px;
+                padding-right: 150px;
+            }
+        }
     </style>
 </header>
 
 <body>
 <?php require 'header.php'; ?>
 
-<div id="tienda_apartado" class="d-grid gap-3" style="grid-template-columns: 1fr 1fr 1fr 1fr;">
+<div id="tienda_apartado">
     
     <!-- Proveedores Section -->
     <div class="apartado">
@@ -89,6 +99,7 @@
                 <a href="addprov.php"><i class="fa-solid fa-file-circle-plus"></i></a>
                 <a href="editprov.php"><i class="fa-regular fa-pen-to-square"></i></a>
                 <a href="deleteprov.php"><i class="fa-solid fa-trash-can"></i></a>
+                <a href="deleteprov.php"><i class="fa-solid fa-file-circle-check"></i></a>
         </div>
     </div>
 
@@ -100,6 +111,7 @@
                 <a href="a.php"><i class="fa-solid fa-file-circle-plus"></i></a>
                 <a href="c.php"><i class="fa-regular fa-pen-to-square"></i></a>
                 <a href="b.php"><i class="fa-solid fa-trash-can"></i></a>
+                <a href="deleteprov.php"><i class="fa-solid fa-file-circle-check"></i></a>
         </div>
     </div>
 
@@ -111,6 +123,7 @@
                 <a href="adddesc.php"><i class="fa-solid fa-file-circle-plus"></i></a>
                 <a href="editdesc.php"><i class="fa-regular fa-pen-to-square"></i></a>
                 <a href="deletedesc.php"><i class="fa-solid fa-trash-can"></i></a>
+                <a href="deleteprov.php"><i class="fa-solid fa-file-circle-check"></i></a>
         </div>
     </div>
 
